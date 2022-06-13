7e9eb453774138b1ef195034a85aa0f1ae4e5945
Saad Al Majayda <s.majayda.c@energy.gov.sa>
2022-06-13 15:54:16 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-layout>
+    <v-flex>
+       <v-card-title class="headline">Welcome to the News API Home Page</v-card-title>
+        <v-btn dark to="/news" nuxt> GO TO NEWS </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
---
+++ pages/news.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-container>
+      <v-flex>
+      </v-flex>
+      <v-flex>
+        <v-flex xs12 pb-3 sm6 offset-sm3 v-for="(article, index) in articles" :key="index">
+          <v-card>
+            <img :src="article.urlToImage" width="100%" height="200px">
+
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{article.title}}</div>
+              </div>
+            </v-card-title>
+            <v-card-text>Published At: {{article.publishedAt}}</v-card-text>
+            <v-card-text>{{article.content}}</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn dark :href="article.url" target="_blank">URL</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-flex>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const { articles } = await app.$axios.$get(
+      "https://newsapi.org/v2/everything?q=tesla&from=2022-05-13&sortBy=publishedAt&apiKey=d94bc71c72f14eca9419bfde30ae72b7"
+    );
+    return { articles };
+  }
+};
+</script>
